5e757ec730957f18931e4c5a4f261ddbb7520cb1
Yoan Roullard <yoan@yoan-Satellite-P300>
2012-12-23 16:04:36 +0100

    Fix bug on multiple timeline in same page. Start Readme installation tutorial

--- README.md
+++ README.md
@@ -9,12 +9,32 @@ JSLightTimeline is a very simple JQuery plugin for create timeline.
 Requirements
 ------------
 
-JSLightTimeline require Twitter Boostrap's css and javscript. 
+JSLightTimeline require :  
+      + Twitter Bootstrap's css and javscript 2.2.2  
+      + JQuery >=1.8.3  
 
 Installation
 ------------
+1. Make sure you have include all required dependencies to your page  
+		```html
+		<head>  
+			<link type="text/css" href="assets/bootstrap/css/bootstrap.css" rel="stylesheet" media="screen" />    
+        		<link type="text/css" href="assets/jsLightTimeline.css" rel="stylesheet" media="screen" />  
+		</head>  
+		<body>		
+			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>  
+        			<script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>  
+        			<script type="text/javascript" src="js/jsLightTimeline.js"></script>  
+		</body>
+		```
+2. Add a div element to your page and add some jquery   
+```html
+<script>
 
-//TODO
+	var camera, scene, renderer;
+	var geometry, material, mesh;
+</script>
+```  
 
 Documentation
 -------------
--- js/jsLightTimeline.js
+++ js/jsLightTimeline.js
@@ -16,34 +16,37 @@
            
            
            var timeline = this;
+           console.log($(timeline).attr('id'));
            var cpt = 0;
            $.each(params.dots,function(){
                if( cpt == 0){
                    if( params.orientation == 'horizontal'){
-                       timeline.append("<div id='dot"+cpt+"' class='dot-"+params.orientation+" dot dotStart-"+params.orientation+"' ><span class='date date-"+params.orientation+"'>"+this.date+"</span></div>")
+                       timeline.append("<div id='"+$(timeline).attr('id')+"-dot"+cpt+"' class='dot-"+params.orientation+" dot dotStart-"+params.orientation+"' ><span class='date date-"+params.orientation+"'>"+this.date+"</span></div>")
                    }
                    else{
-                       timeline.append("<div id='dot"+cpt+"' class='dot-"+params.orientation+" dot dotEnd-"+params.orientation+" ' ><span class='date date-"+params.orientation+"'>"+this.date+"</span></div>")
+                       timeline.append("<div id='"+$(timeline).attr('id')+"-dot"+cpt+"' class='dot-"+params.orientation+" dot dotEnd-"+params.orientation+" ' ><span class='date date-"+params.orientation+"'>"+this.date+"</span></div>")
                    }
                     
                }
                else if ( cpt == params.dots.length-1 ){
                    if( params.orientation == 'horizontal'){
-                       timeline.append("<div id='dot"+cpt+"' class='dot-"+params.orientation+" dot dotEnd-"+params.orientation+"' ><span class='date date-"+params.orientation+"' >"+this.date+"</span></div>")
+                       timeline.append("<div id='"+$(timeline).attr('id')+"-dot"+cpt+"' class='dot-"+params.orientation+" dot dotEnd-"+params.orientation+"' ><span class='date date-"+params.orientation+"' >"+this.date+"</span></div>")
                    }else{
-                      timeline.append("<div id='dot"+cpt+"' class='dot-"+params.orientation+" dot dotEnd-"+params.orientation+"' ><span class='date date-"+params.orientation+"'>"+this.date+"</span></div>")
+                      timeline.append("<div id='"+$(timeline).attr('id')+"-dot"+cpt+"' class='dot-"+params.orientation+" dot dotEnd-"+params.orientation+"' ><span class='date date-"+params.orientation+"'>"+this.date+"</span></div>")
                     
                    }
                    
                }
                else{
-                   timeline.append("<div id='dot"+cpt+"' class='dot-"+params.orientation+" dot' ><span class='date date-"+params.orientation+"'>"+this.date+"</span></div>")
+                   timeline.append("<div id='"+$(timeline).attr('id')+"-dot"+cpt+"' class='dot-"+params.orientation+" dot' ><span class='date date-"+params.orientation+"'>"+this.date+"</span></div>")
                    
                }
+               
                if( params.orientation == 'horizontal'){                   
                    this.placement = "top";
                }
-               $("#timeline #dot"+cpt).popover(this);
+               console.log("#"+$(timeline).attr('id')+"-dot"+cpt);
+               $("#"+$(timeline).attr('id')+"-dot"+cpt).popover(this);
                
                cpt ++;
                
